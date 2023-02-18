c53e244b1f7eaff4e1e275ca77ec2df01fc23dc4
Attila GOLONCSER <agl@kxxp.net>
2023-02-18 12:05:34 +0100

    Upgrade Iosevka to v19.0.0

--- fonts/iosevka.sh
+++ fonts/iosevka.sh
@@ -4,6 +4,6 @@ set -eu
 set -xv
 dir=$(mktemp -d)
 
-curl -L "https://github.com/be5invis/Iosevka/releases/download/v16.2.0/ttf-iosevka-16.2.0.zip" -o "$dir/iosevka.zip"
+curl -L "https://github.com/be5invis/Iosevka/releases/download/v19.0.0/ttf-iosevka-16.2.0.zip" -o "$dir/iosevka.zip"
 unzip -x "$dir/iosevka.zip" -d "$dir"
 find "$dir" -type f -name "*.ttf" | xargs -I {} cp -v {} "${FONTDIR}/"
